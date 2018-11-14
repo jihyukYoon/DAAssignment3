a2264d30cf4a344ff57a0af749e4c31a30f660d7
iamjustinjp <jihyuk328@gmail.com>
2018-11-14 15:26:29 -0800

    Added Controller and Class

---
+++ ANJYAssignment3/ANJYAssignment3/Controllers/FoodsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ANJYAssignment3.Controllers
+{
+    public class FoodsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
