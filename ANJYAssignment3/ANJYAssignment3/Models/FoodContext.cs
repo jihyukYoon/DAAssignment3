e015cf8ea5ff5cdcaa8964dafb375198f860cf60
iamjustinjp <jihyuk328@gmail.com>
2018-11-14 15:30:40 -0800

    Editted Classes

--- ANJYAssignment3/ANJYAssignment3/Models/FoodContext.cs
+++ ANJYAssignment3/ANJYAssignment3/Models/FoodContext.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace ANJYAssignment3.Models
 {
-    public class FoodContext
+    public class FoodContext : DbContext
     {
+        public FoodContext(DbContextOptions<FoodContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Foods> Foods { get; set; }
     }
 }
--- ANJYAssignment3/ANJYAssignment3/Models/Foods.cs
+++ ANJYAssignment3/ANJYAssignment3/Models/Foods.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace ANJYAssignment3.Models
 {
     public class Foods
     {
+        [Key]
+        public long Food_id { get; set; }
+        public string FoodName { get; set; }
+        public string recipe { get; set; }
+        public string description { get; set; }
+
     }
 }
